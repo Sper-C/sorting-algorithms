4b28efb6ca515dbaba5745c83d60f4ef144ca249
marucube35 <marucube35@gmail.com>
2021-11-29 12:21:16 +0700

    init branch

--- driver.cpp
+++ driver.cpp
@@ -1,69 +1,10 @@
-#include "heap.h"
 
-int randomNumber(int n)
+#include "header.h"
+
+int main(int argc, const char *argv[])
 {
-    return rand() % (n - 1 + 1) + 1;
-}
-
-void printArray(int *a, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-}
-
-int *initRandomArray(int &n)
-{
-    cout << "How many elements?" << endl;
-    cin >> n;
-    cout << "Size of element?" << endl;
-    int size;
-    cin >> size;
-    int *arr = new int[n];
-
-    srand(time(0));
-
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = randomNumber(size);
-    }
-    return arr;
-}
-
-int *initInputArray(string file, int &n)
-{
-    fstream f(file, ios::in);
-
-    string size;
-    getline(f, size);
-    n = stoi(size);
-    int *arr = new int[n];
-
-    for (int i = 0; i < n; i++)
-    {
-        string num;
-        f >> num;
-        if (num != " ")
-        {
-            arr[i] = stoi(num);
-        }
-    }
-
-    f.close();
-    return arr;
-}
-
-int main(int agrc, char **argv)
-{
-    int n;
-    int *a = initRandomArray(n);
-
-    cout<<"-------------"<<endl;
-    cout<<"Run Time: "<<measureHeapTime(a,n)<<endl;
-    cout<<"Comparison: "<<measureHeapComp(a,n)<<endl;
-
+    cmd4(argc,argv);
+    cmd5(argc,argv);
     system("pause");
-    return 0;
+    return 1;
 }

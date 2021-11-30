45eb055446400b436ef1a45c65daf47332282a15
toanle312 <toanle3122002@gmail.com>
2021-11-30 20:04:34 +0700

    Write comment

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -3,17 +3,27 @@
 #include <algorithm>
 using namespace std;
 using namespace std::chrono;
+/*
+     Tham sồ đầu vào của hàm:
+     - Mảng: a[]
+     - Số lượng phần tử trong mảng: n
+*/
 void insertionSort(int a[], int n)
 {
+     // Chạy từ i=1 vì i=0 được xem như đã gán vào dãy con đã được sắp xếp
      for (int i = 1; i < n; i++)
      {
+          // Lưu lại phần tử ở vị trí đang xét
           int key = a[i];
+          // Xét dãy con đã được sắp xếp chạy từ 0 đến i-1
           int j = i - 1;
+          // So sánh và tìm vị trí để chèn key vào dãy con đã được sắp xếp
           while (j >= 0 && key < a[j])
           {
                a[j + 1] = a[j];
                j--;
           }
+          // chèn phần tử key vào mảng con đã được sắp xếp
           a[j + 1] = key;
      }
      for (int i = 0; i < n; i++)

a136fef157c5810e69fe1223e37bebe38f6c9f18
marucube35 <marucube35@gmail.com>
2021-11-30 08:45:28 +0700

    add cmts

--- heapSort.cpp
+++ heapSort.cpp
@@ -6,12 +6,13 @@ void heapify(int *arr, int n, int i)
     int l = 2 * i + 1; 
     int r = 2 * i + 2; 
 
+    //*So sánh với node con trái
     if (l < n && arr[l] > arr[largest])
         largest = l;
-
+    //*với node con phải
     if (r < n && arr[r] > arr[largest])
         largest = r;
-
+    //*Đổi chỗ với node con và tiến hành heapify ở node con.
     if (largest != i)
     {
         swap(arr[i], arr[largest]);
@@ -20,13 +21,14 @@ void heapify(int *arr, int n, int i)
     }
 }
 
-int heapSort(int *arr, int n)
+void heapSort(int *arr, int n)
 {
+    //*Xây dựng max heap
     for (int i = n / 2 - 1; i >= 0; i--)
     {
         heapify(arr, n, i);
     }
-
+    //*Sắp xếp kèm heapify
     for (int i = n - 1; i > 0; i--)
     {
         swap(arr[0], arr[i]);
